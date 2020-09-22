c24d0840bdb01f267fea31798feee9467262620f
Mark Moniz <megglymark@gmail.com>
2020-09-21 21:43:56 -0700

    Add foundation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 20rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Hello = styled.div`
+  font-size: 3rem;
+`;
 
 export default function Home() {
-  return <div>Hello world!</div>
-}
+  return (
+    <Container>
+      <title>Hi</title>
+      <Hello>Hi</Hello>
+    </Container>
+  );
+};
